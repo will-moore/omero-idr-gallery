768fabe702ec46fa0abdf39d9eb24fe511183f1e
William Moore <w.moore@dundee.ac.uk>
2018-08-11 16:56:19 +0100

    App pure functional component

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,13 @@
-import React, { Component } from 'react';
+import React from 'react';
 import logo from './logo-idr.svg';
 import './App.css';
 import Projects from './containers/Projects';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-        </header>
-        <Projects />
-      </div>
-    );
-  }
-}
-
-export default App;
+export default () => (
+  <div className="App">
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+    </header>
+    <Projects />
+  </div>
+);
