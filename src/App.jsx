0c1548f18c5fb02c6acda8ae31feffdd181305c1
William Moore <w.moore@dundee.ac.uk>
2018-08-14 22:39:50 +0100

    App has loading state to show idr-logo .loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 import IdrLogo from './svg/IdrLogo';
 import './App.css';
 import Projects from './containers/Projects';
 
-export default () => (
-  <div className="App">
-    <header className="App-header loading">
-      <IdrLogo />
-    </header>
-    <Projects />
-  </div>
-);
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: false,
+    };
+    this.setLoading = this.setLoading.bind(this);
+  }
+
+  setLoading(loading) {
+    this.setState({ loading });
+  }
+
+  render() {
+    const { loading } = this.state;
+    const l = loading ? 'loading' : '';
+    return (
+      <div className="App">
+        <header className={`App-header ${l}`}>
+          <IdrLogo />
+        </header>
+        <Projects setLoading={this.setLoading} />
+      </div>
+    );
+  }
+}
+
+export default App;
--- src/containers/Projects.jsx
+++ src/containers/Projects.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import fetchJsonp from 'fetch-jsonp';
 import UlGroup from '../lists/UlGroup';
 import LiGroupItem from '../lists/LiGroupItem';
@@ -12,9 +13,12 @@ class Projects extends Component {
   }
 
   componentDidMount() {
+    const { setLoading } = this.props;
+    setLoading(true);
     fetchJsonp('http://idr.openmicroscopy.org/webgateway/proj/list/')
       .then(response => response.json())
       .then((projects) => {
+        setLoading(false);
         projects.sort((a, b) => {
           if (a.name < b.name) return -1;
           if (a.name > b.name) return 1;
@@ -47,5 +51,9 @@ Loading Studies...
     );
   }
 }
+
+Projects.propTypes = {
+  setLoading: PropTypes.func.isRequired,
+};
 
 export default Projects;
